80e94a1e94a8af976f469a325fb23a49ccbb9d3a
Pin Shan Chuang <pinshan.chuang@104.com.tw>
2024-01-27 18:48:18 +0800

    fix: shortened url

--- api_tool/actions.py
+++ api_tool/actions.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
 from . import crud
@@ -11,7 +12,12 @@ def get_original_url(
         shortened_url=shortened_url,
         db=db
     )
-    return original_url
+    if not original_url:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="網址不存在"
+        )
+    return original_url.link
 
 
 def get_shortened_url(
@@ -23,7 +29,7 @@ def get_shortened_url(
         db=db
     )
     if shortened_url:
-        return shortened_url
+        return shortened_url.key
     shortened_url = crud.create_shortened_url(
         url=url,
         db=db
